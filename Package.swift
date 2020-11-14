12c97c1f6fce8b36cf95233a187a4cb6fdc32460
Christian Treffs <ctreffs@gmail.com>
2020-11-14 03:28:13 +0100

    Rename

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftMiniaudio",
+    name: "Miniaudio",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftMiniaudio",
-            targets: ["SwiftMiniaudio"])
+            name: "Miniaudio",
+            targets: ["Miniaudio"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftMiniaudio",
+            name: "Miniaudio",
             dependencies: ["CMiniaudio"]),
 
         .target(name: "CMiniaudio"),
-        .target(name: "SwiftMiniaudioApp",
-                dependencies: [.byName(name: "SwiftMiniaudio")])
+        .target(name: "MiniaudioApp",
+                dependencies: [.byName(name: "Miniaudio")])
     ]
 )
